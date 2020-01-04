c083e34b1a2e0b54f1b449b6f2d86bcef9d4831b
zhangrenzheng <renzheng.zh@gmail.com>
2020-01-04 22:34:33 +0800

    对接用户登录

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况,虽然我知道用途，但是不知道用法
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
@@ -29,6 +47,7 @@ App({
   },
 
   globalData: {
-    openID: "100000000151"
+    openID: "",
+    userInfo: null,
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -10,14 +10,48 @@ Page({
     tab: 0,
     displayAllDevice: false,
     openID: app.globalData.openID,
-    deviceList: []
+    deviceList: [],
+    phone: '',
+
+    // 用户登录，三个变量分别是用户登录信息，用户是否登录，该函数能否使用
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+
+    showInputPhoneDialogVisible: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 用户登录,先判断用户是否登录
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 执行一个回调函数，因为可能getUserInfo会在page.onload之后才返回，回调函数可以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 兼容性处理，需要注意的是微信基本放弃了wx.getUserInfo将不再支持，所以这个部分没用
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
@@ -31,18 +65,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that = this;
-    var openID = this.data.openID;
-
-    /* 获取设备列表 */
-    wx.request({
-      url: 'https://swv.wuwz.net/UserDevices?openID=' + encodeURIComponent(openID),
-      success: function (res) {
-        that.setData({
-          deviceList: res.data
-        })
-      }
-    });
+    if (!this.data.openID)
+      return
+    this.requestDeviceList();
   },
 
   /**
@@ -77,7 +102,11 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: '智能水阀微信小程序',
+      path: '/pages/index/index',
+      // 不再支持回调函数
+    }
   },
 
   displayAllDevice: function (e) {
@@ -85,6 +114,89 @@ Page({
     this.setData({
       displayAllDevice: (display == true ? false : true)
     })
+  },
+
+  /* 请求登录 */
+  requestLogIn: function () {
+    var that = this;
+    var openID = this.data.openID;
+
+    if (!openID)
+      return
+
+    wx.request({
+      url: 'https://swv.wuwz.net/UserLogin?openID=' + encodeURIComponent(openID),
+      success: function (res) {
+        if (res.data.phone == 0) {
+          that.setData({
+            showInputPhoneDialogVisible: true
+          })
+        }
+        else that.requestDeviceList()
+      }
+    });
+  },
+
+
+  /* 用户注册输入手机号弹窗相关函数 */
+
+  cancelInputPhone: function () {
+    this.setData({
+      showInputPhoneDialogVisible: false
+    });
+  },
+
+  inputPhone: function (e) {
+    this.setData({
+      phone: e.detail.value
+    });
+  },
+
+  requestRegisterAccount: function () {
+    var that = this;
+    var openID = this.data.openID;
+    var phone = this.data.phone;
+
+    wx.request({
+      url: 'https://swv.wuwz.net/UserRegister?openID=' + encodeURIComponent(openID)
+        + '&phone=' + encodeURIComponent(phone) + '&imgUrl=https://zkk.me/favicon.png',
+      success: function (res) {
+        if (res.data.result == 1)
+        {
+          console.log("注册用户成功")
+          that.requestDeviceList()
+        }
+        if (res.data.result == 0)
+          console.log("注册用户失败")
+      }
+    });
+  },
+
+  confirmRegister: function () {
+    if (!this.data.phone)
+      return
+    this.requestRegisterAccount()
+    this.setData({
+      showInputPhoneDialogVisible: false
+    })
+  },
+
+  /* 获取设备列表 */
+  requestDeviceList: function () {
+    var that = this;
+    var openID = this.data.openID;
+
+    if (!openID)
+      return
+
+    wx.request({
+      url: 'https://swv.wuwz.net/UserDevices?openID=' + encodeURIComponent(openID),
+      success: function (res) {
+        that.setData({
+          deviceList: res.data
+        })
+      }
+    });
   },
 
   /* 禁止 swiper 手动滑动 */
@@ -142,5 +254,62 @@ Page({
     this.setData({
       tab: e.detail.current
     })
+  },
+
+  // 登录按钮
+  getUserInfo: function (e) {
+    var that = this;
+
+    if(e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    }
+    else {
+      wx.showModal({
+        title: '授权失败',
+        content: '注意：不通过授权，多数功能将无法使用',
+        success(res) {
+          if(res.confirm || res.cancel) {
+            return
+          }
+        }
+      })
+    }
+
+    wx.login({
+      success:function(res){
+        wx.request({
+          url: 'https://api.ijio.net/getOpenId.php',
+          data: {code:res.code},
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function (res) {
+            console.log(res.data.openid)
+            app.globalData.openID = res.data.openid
+            that.setData({
+              openID: res.data.openid
+            })
+            that.requestLogIn();
+            //that.requestDeviceList();
+          }
+        })
+      }
+    })
+  },
+  // 退出登录
+  logOut: function(){
+    app.globalData.userInfo = {}
+    app.globalData.openID = {}
+    this.setData({
+      hasUserInfo: false,
+      userInfo: {},
+      openID: '',
+      deviceList: []
+    })
+    wx.clearStorage()
   }
 })
